9209b0074ed59e72763387c3f74c15ec0cacb690
IlhamElkasm <elkasmiilham994@gmail.com>
2025-03-12 11:01:43 +0000

    create un projet back end de newtech

---
+++ NewTechBackEnd/src/main/java/com/newTech/NewTechApplication.java
@@ -0,0 +1,13 @@
+package com.newTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewTechApplication.class, args);
+	}
+
+}
---
+++ NewTechBackEnd/src/test/java/com/newTech/NewTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
