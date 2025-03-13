bbf186d7d1ce06d532269049a5a9882c09a681b9
IlhamElkasm <elkasmiilham994@gmail.com>
2025-03-13 10:18:38 +0000

    creation de  repository de NotificationRepository

---
+++ NewTechBackEnd/src/main/java/com/newTech/Repositories/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.newTech.Repositories;
+
+import com.newTech.Model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {}
+
